dfb792f03361bd46fa88f9b7ed8e6f77eaee67a4
Samuil Georgiev <sami.georgiev@gmail.com>
2018-05-23 00:26:52 +0300

    table fixed

--- reservation.php
+++ reservation.php
@@ -6,21 +6,27 @@
 	<title>Central West Hotel </title>
 </head>
 <body>
-	<img style="height:500px" src="./logo.png">
-	<?php
-		$from = $_GET["from"];
-		$to = $_GET["to"];
-		$adults = $_GET["adults"];
-		$children = $_GET["children"];
-		echo "<br><table>";
-		echo "<tr><td>Check-in date </td> <td><input type=\"text\" name=\"from_new\" value=\"$from\"></td></tr>";
-		echo "<tr><td>Check-out date </td> <td><input type=\"text\" name=\"to_new\" value=\"$to\"></td></tr>";
-		echo "<tr><td>Adults </td> <td><input type=\"text\" name=\"adults_new\" value=\"$adults\"></td></tr>";
-		echo "<tr><td>Children </td> <td><input type=\"text\" name=\"children_new\" value=\"$children\"></td></tr>";
-		echo "<tr><td>Name </td> <td><input type=\"text\" name=\"cname\"></td></tr>";
-		echo "<tr><td>Phone </td> <td><input type=\"text\" name=\"phone\"></td></tr>";
-		echo "<tr><td>Email </td> <td><input type=\"text\" name=\"email\"></td></tr>";
-		echo "</table>";
-	?>
+	<div>
+		<div style="height: 500px">
+			<img style="height:500px" src="./logo.png">
+		</div>
+		<div>
+			<?php
+				$from = $_GET["from"];
+				$to = $_GET["to"];
+				$adults = $_GET["adults"];
+				$children = $_GET["children"];
+				echo "<br><table>";
+				echo "<tr><td>Check-in date </td> <td><input type=\"text\" name=\"from_new\" value=\"$from\"></td></tr>";
+				echo "<tr><td>Check-out date </td> <td><input type=\"text\" name=\"to_new\" value=\"$to\"></td></tr>";
+				echo "<tr><td>Adults </td> <td><input type=\"text\" name=\"adults_new\" value=\"$adults\"></td></tr>";
+				echo "<tr><td>Children </td> <td><input type=\"text\" name=\"children_new\" value=\"$children\"></td></tr>";
+				echo "<tr><td>Name </td> <td><input type=\"text\" name=\"cname\"></td></tr>";
+				echo "<tr><td>Phone </td> <td><input type=\"text\" name=\"phone\"></td></tr>";
+				echo "<tr><td>Email </td> <td><input type=\"text\" name=\"email\"></td></tr>";
+				echo "</table>";
+			?>
+		</div>
+	</div>	
 </body>
 </html>
