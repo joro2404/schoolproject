e3a04789efe6722505fd3a7dffe7d2227fb0a2f0
Samuil Georgiev <sami.georgiev@gmail.com>
2018-05-23 00:13:48 +0300

    table added

--- reservation.php
+++ reservation.php
@@ -13,9 +13,9 @@
 		$adults = $_GET["adults"];
 		$children = $_GET["children"];
 		echo "<table>";
-		echo "<tr><td>Check-in date</td><td> <input type=\"text\" name=\"from_new\" value=\"$from\">";
-		echo "Check-out date <input type=\"text\" name=\"to_new\" value=\"$to\">";
-		echo "Adults <input type=\"text\" name=\"adults_new\" value=\"$adults\">";
+		echo "<tr><td>Check-in date </td> <td><input type=\"text\" name=\"from_new\" value=\"$from\"></td></tr>";
+		echo "<tr><td>Check-out date </td> <td><input type=\"text\" name=\"to_new\" value=\"$to\"></td></tr>";
+		echo "<tr><td>Adults </td> <td><input type=\"text\" name=\"adults_new\" value=\"$adults\"></td></tr>";
 		echo "Check-in date <input type=\"text\" name=\"children_new\" value=\"$children\">";
 		echo "Name<input type=\"text\" name=\"cname\">";
 		echo "Phone<input type=\"text\" name=\"phone\">";
