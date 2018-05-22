7fcc17a10680c9d02e7c02b84ea9603057fc9e79
Samuil Georgiev <sami.georgiev@gmail.com>
2018-05-23 00:04:09 +0300

    git fixed

--- reservation.php
+++ reservation.php
@@ -11,7 +11,13 @@
 		$to = $_GET["to"];
 		$adults = $_GET["adults"];
 		$children = $_GET["children"];
-		echo "Запитване за $adults и $children от $from до $to";
+		echo "Check-in date<input type=\"text\" name=\"from_new\" value=\"$from\">";
+		echo "Check-out date<input type=\"text\" name=\"to_new\" value=\"$to\">";
+		echo "Adults<input type=\"text\" name=\"adults_new\" value=\"$adults\">";
+		echo "Check-in date<input type=\"text\" name=\"children_new\" value=\"$children\">";
+		echo "Name<input type=\"text\" name=\"cname\">";
+		echo "Phone<input type=\"text\" name=\"phone\">";
+		echo "Email<input type=\"text\" name=\"email\">";
 	?>
 </body>
 </html>>
