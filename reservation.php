81b8a0d5c98c4dbadb02eecf8832de3f5d6dbb08
Samuil Georgiev <sami.georgiev@gmail.com>
2018-05-22 21:36:51 +0300

    res.php

---
+++ reservation.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Central West Hotel </title>
+</head>
+<body>
+	<?php
+		$from = $_GET["from"];
+		$to = $_GET["to"];
+		$adults = $_GET["adults"];
+		$children = $_GET["children"];
+		echo "Запитване за $adults и $children от $from до $to";
+	?>
+</body>
+</html>>
