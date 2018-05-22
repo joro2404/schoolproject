8eb58b3a09ecf2666adaa5277d5e9f992942514b
Samuil Georgiev <sami.georgiev@gmail.com>
2018-05-23 00:23:06 +0300

    table fixed

--- reservation.php
+++ reservation.php
@@ -6,20 +6,21 @@
 	<title>Central West Hotel </title>
 </head>
 <body>
-	<img src="./logo.png">
+	<img style="height:500px" src="./logo.png">
 	<?php
 		$from = $_GET["from"];
 		$to = $_GET["to"];
 		$adults = $_GET["adults"];
 		$children = $_GET["children"];
-		echo "<table>";
+		echo "<br><table>";
 		echo "<tr><td>Check-in date </td> <td><input type=\"text\" name=\"from_new\" value=\"$from\"></td></tr>";
 		echo "<tr><td>Check-out date </td> <td><input type=\"text\" name=\"to_new\" value=\"$to\"></td></tr>";
 		echo "<tr><td>Adults </td> <td><input type=\"text\" name=\"adults_new\" value=\"$adults\"></td></tr>";
-		echo "Check-in date <input type=\"text\" name=\"children_new\" value=\"$children\">";
-		echo "Name<input type=\"text\" name=\"cname\">";
-		echo "Phone<input type=\"text\" name=\"phone\">";
-		echo "Email<input type=\"text\" name=\"email\">";
+		echo "<tr><td>Children </td> <td><input type=\"text\" name=\"children_new\" value=\"$children\"></td></tr>";
+		echo "<tr><td>Name </td> <td><input type=\"text\" name=\"cname\"></td></tr>";
+		echo "<tr><td>Phone </td> <td><input type=\"text\" name=\"phone\"></td></tr>";
+		echo "<tr><td>Email </td> <td><input type=\"text\" name=\"email\"></td></tr>";
+		echo "</table>";
 	?>
 </body>
 </html>
