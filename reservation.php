756dad38ab4b5c4ec6c138888e7edf8e59546442
Samuil Georgiev <sami.georgiev@gmail.com>
2018-05-23 00:10:27 +0300

    nothing

--- reservation.php
+++ reservation.php
@@ -12,13 +12,14 @@
 		$to = $_GET["to"];
 		$adults = $_GET["adults"];
 		$children = $_GET["children"];
-		echo "Check-in date<input type=\"text\" name=\"from_new\" value=\"$from\">";
-		echo "Check-out date<input type=\"text\" name=\"to_new\" value=\"$to\">";
-		echo "Adults<input type=\"text\" name=\"adults_new\" value=\"$adults\">";
-		echo "Check-in date<input type=\"text\" name=\"children_new\" value=\"$children\">";
+		echo "<table>";
+		echo "<tr><td>Check-in date</td><td> <input type=\"text\" name=\"from_new\" value=\"$from\">";
+		echo "Check-out date <input type=\"text\" name=\"to_new\" value=\"$to\">";
+		echo "Adults <input type=\"text\" name=\"adults_new\" value=\"$adults\">";
+		echo "Check-in date <input type=\"text\" name=\"children_new\" value=\"$children\">";
 		echo "Name<input type=\"text\" name=\"cname\">";
 		echo "Phone<input type=\"text\" name=\"phone\">";
 		echo "Email<input type=\"text\" name=\"email\">";
 	?>
 </body>
-</html>>
+</html>
