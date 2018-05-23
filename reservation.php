d487db1262bdd264dc6dcfb9183d1fae75dcc634
Samuil Georgiev <sami.georgiev@gmail.com>
2018-05-23 10:30:12 +0300

    ajax added

--- reservation.php
+++ reservation.php
@@ -4,6 +4,11 @@
 <head>
 	<meta charset="utf-8">
 	<title>Central West Hotel </title>
+	<script type="text/javascript">
+		var xhhtp = new XMLHttpRequest();
+		xhttp.open("GET", "./reservation.php", true);
+
+	</script>>
 </head>
 <body>
 	<div>
@@ -16,8 +21,8 @@
 				$to = $_GET["to"];
 				$adults = $_GET["adults"];
 				$children = $_GET["children"];
-				echo "<br><table>";
-				echo "<tr><td>Check-in date<p style=\"color:red\">* </p> </td> <td><input type=\"text\" name=\"from_new\" value=\"$from\"></td></tr>";
+				echo "<form action=\"reservation.php\"><br><table>";
+				echo "<tr><td>Check-in date </td> <td><input type=\"text\" name=\"from_new\" value=\"$from\"></td></tr>";
 				echo "<tr><td>Check-out date </td> <td><input type=\"text\" name=\"to_new\" value=\"$to\"></td></tr>";
 				echo "<tr><td>Adults </td> <td><input type=\"text\" name=\"adults_new\" value=\"$adults\"></td></tr>";
 				echo "<tr><td>Children </td> <td><input type=\"text\" name=\"children_new\" value=\"$children\"></td></tr>";
@@ -26,6 +31,10 @@
 				echo "<tr><td>Email </td> <td><input type=\"text\" name=\"email\"></td></tr>";
 				echo "</table>";
 			?>
+			<p id="demo"></p>
+			<script>
+				document.getElementByID('demo').innerHTML = document.getElementsByTagName('adults');
+			</script>
 		</div>
 	</div>	
 </body>
