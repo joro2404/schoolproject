b11dec6b2f03f274ac8beca40d1ce02c1a3df5cd
Samuil Georgiev <sami.georgiev@gmail.com>
2018-05-31 00:02:04 +0300

    nthing

--- reservation.php
+++ reservation.php
@@ -18,13 +18,13 @@
     		var date1 = document.getElementById("from_new").value, date2 = document.getElementById("to_new").value, adults = document.getElementById("adults_new").value;
     		var children = document.getElementById("children_new").value, name = document.getElementById("cname").value, phone = document.getElementById("phone").value;
 			var msg = "Reservation from website: ESCnCheck-in date: " + date1 + "ESCnCheck-out date: " + date2 + "ESCnAdults: " + adults + "ESCnChildren: " + children + "ESCnName: " + name + "ESCnPhone: " + phone + "ESCnE-mail: " + document.getElementById("email").value;
-			xhttp.open("GET", "http://ifko42.info/sami/send_mail.php?msg=" + msg.replace("/", "%2F") + "&" + g-recaptcha.getResponse(), true);
+			xhttp.open("GET", "http://ifko42.info/sami/send_mail.php?msg=" + msg.replace("/", "%2F"), true);
 			xhttp.send();
 		}
 	</script>
 </head>
 <body>
-<!--	<div class="colorlib-loader"></div>
+	<div class="colorlib-loader"></div>
 
 	<div id="page">
 		<nav class="colorlib-nav" role="navigation">
@@ -69,7 +69,7 @@
 				</div>
 			</div>
 		</nav>
--->
+
 
 			<?php
 				$from = $_GET["from"];
