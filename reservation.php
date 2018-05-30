2866088ffe60517b37340b8d80b23c936680e351
Samuil Georgiev <sami.georgiev@gmail.com>
2018-05-30 23:19:45 +0300

    Captcha added

--- reservation.php
+++ reservation.php
@@ -5,61 +5,28 @@
 	<meta charset="utf-8">
 	<meta Access-Control-Allow-Origin: *>
 	<title>Central West Hotel </title>
+	<script src='https://www.google.com/recaptcha/api.js'></script>
 	<script type="text/javascript">
 		function send() {
 		//	document.getElementById("butt").disabled = true;
 			var xhttp = new XMLHttpRequest();
 			xhttp.onreadystatechange = function () {
 				if (this.readyState == 4 && this.status == 200) {
-      				document.getElementById("demo").innerHTML = this.responseText;
+					alert("Successfully sent");
     			}
     		};
     		var date1 = document.getElementById("from_new").value, date2 = document.getElementById("to_new").value, adults = document.getElementById("adults_new").value;
     		var children = document.getElementById("children_new").value, name = document.getElementById("cname").value, phone = document.getElementById("phone").value;
-    		if (isNaN(Date.parse(date1)) || isNaN(Date.parse(date2)) || !(Number.isInteger(children)) || !(Number.isInteger(adults))) alert ("error");
+    	//	if (isNaN(Date.parse(date1)) || isNaN(Date.parse(date2)) || !(Number.isInteger(children)) || !(Number.isInteger(adults))) alert ("error");
 			var msg = "Reservation from website: ESCnCheck-in date: " + date1 + "ESCnCheck-out date: " + date2 + "ESCnAdults: " + adults + "ESCnChildren: " + children + "ESCnName: " + name + "ESCnPhone: " + phone + "ESCnE-mail: " + document.getElementById("email").value;
 			//var par = "?" + "from=" + document.getElementById('from_new').value.replace("/", "%2F") + "&to=" + document.getElementById("to_new").value.replace("/", "%2F") + "&adults=" + document.getElementById("adults_new").value + "&children=" + document.getElementById("children_new") + "&name=" + document.getElementById("cname").replace(" ", "%32") + "&email=" + document.getElementById("email") + "&phone=" + document.getElementById("phone");
 			xhttp.open("GET", "http://ifko42.info/sami/send_mail.php?msg=" + msg.replace("/", "%2F"), true);
 			xhttp.send();
-			alert("Successfully sent");
 		}
 	</script>
-
-	<!-- Animate.css -->
-	<link rel="stylesheet" href="css/animate.css">
-	<!-- Icomoon Icon Fonts-->
-	<link rel="stylesheet" href="css/icomoon.css">
-	<!-- Bootstrap  -->
-	<link rel="stylesheet" href="css/bootstrap.css">
-
-	<!-- Magnific Popup -->
-	<link rel="stylesheet" href="css/magnific-popup.css">
-
-	<!-- Flexslider  -->
-	<link rel="stylesheet" href="css/flexslider.css">
-
-	<!-- Owl Carousel -->
-	<link rel="stylesheet" href="css/owl.carousel.min.css">
-	<link rel="stylesheet" href="css/owl.theme.default.min.css">
-
-	<!-- Date Picker -->
-	<link rel="stylesheet" href="css/bootstrap-datepicker.css">
-	<!-- Flaticons  -->
-	<link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
-
-	<!-- Theme style  -->
-	<link rel="stylesheet" href="css/style.css">
-
-	<!-- Modernizr JS -->
-	<script src="js/modernizr-2.6.2.min.js"></script>
-	<!-- FOR IE9 below -->
-	<!--[if lt IE 9]>
-	<script src="js/respond.min.js"></script>
-	<![endif]-->
-
 </head>
 <body>
-	<div class="colorlib-loader"></div>
+<!--	<div class="colorlib-loader"></div>
 
 	<div id="page">
 		<nav class="colorlib-nav" role="navigation">
@@ -70,12 +37,12 @@
 							<p class="site">www.centralwesthotel.com</p>
 						</div> 
 						<div class="col-xs-8 text-right">
-							<!--<p class="num">Call: +359885747606</p>
-							<p class="num">e-mail : contact@centralwesthotel.com</p> -->
+							<p class="num">Call: +359885747606</p>
+							<p class="num">e-mail : contact@centralwesthotel.com</p>
 							<ul class="colorlib-social">
 								<li><a href="https://youtube.com/" target="_blank"><i class="icon-youtube"></i></a></li>
 								<li><a href="https://www.facebook.com/" target = "_blank"><i class="icon-facebook"></i></a></li>
-								<!--<li><a href="#"><i class="icon-linkedin"></i></a></li>-->
+								<li><a href="#"><i class="icon-linkedin"></i></a></li>
 								<li><a href="https://www.instagram.com/?hl=bg" target = "_blank"><i class="icon-instagram"></i></a></li>
 							</ul> 
 						</div>
@@ -104,7 +71,7 @@
 				</div>
 			</div>
 		</nav>
-
+-->
 
 			<?php
 				$from = $_GET["from"];
@@ -119,6 +86,7 @@
 				echo "<tr><td>Name </td> <td><input type=\"text\" id=\"cname\"></td></tr>";
 				echo "<tr><td>Phone </td> <td><input type=\"text\" id=\"phone\"></td></tr>";
 				echo "<tr><td>Email </td> <td><input type=\"text\" id=\"email\"></td></tr>";
+				echo "<tr><td><div class=\"g-recaptcha\" data-sitekey=\"6Ld3vVsUAAAAADphxwRIEewSOnuhA1X0WrJWQo9C\"></div></td></tr>";
 				echo "<tr><td><button id=\"butt\" type=\"button\" onclick=\"send()\">Send</td></tr>";
 				echo "</table></form>";
 			?>
