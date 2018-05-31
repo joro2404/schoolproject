a1e87467286513a4413c9d7a4ad631deab7bed01
Samuil Georgiev <sami.georgiev@gmail.com>
2018-05-31 10:08:06 +0300

    Design fixed

--- reservation.php
+++ reservation.php
@@ -22,6 +22,35 @@
 			xhttp.send();
 		}
 	</script>
+
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="css/flexslider.css">
+
+	<!-- Owl Carousel -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+	<!-- Date Picker -->
+	<link rel="stylesheet" href="css/bootstrap-datepicker.css">
+	<!-- Flaticons  -->
+	<link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+
+	
 </head>
 <body>
 	<div class="colorlib-loader"></div>
@@ -69,7 +98,6 @@
 				</div>
 			</div>
 		</nav>
-
 
 			<?php
 				$from = $_GET["from"];
