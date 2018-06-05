c6d39ce8fd6145a825ff3284a49edce534823827
Samuil Georgiev <sami.georgiev@gmail.com>
2018-06-05 10:56:38 +0300

    nothing

--- reservation.php
+++ reservation.php
@@ -132,6 +132,7 @@
 				echo "<tr><td><button id=\"butt\" type=\"button\" onclick=\"send()\">Send</td></tr>";
 				echo "</table></form>";
 			?>
+
 		
 		<div id="colorlib-contact">
 			<div class="container">
@@ -157,5 +158,47 @@
 							</div>
 						</div>
 					</div>
-</body>
+					<!--<div class="col-md-10 col-md-offset-1 animate-box">
+						<h3>Get In Touch</h3>
+						<form action="#">
+							<div class="row form-group">
+								<div class="col-md-6">
+									<label for="fname">First Name</label>
+									<input type="text" id="fname" class="form-control" placeholder="Your firstname">
+								</div>
+								<div class="col-md-6">
+									<label for="lname">Last Name</label>
+									<input type="text" id="lname" class="form-control" placeholder="Your lastname">
+								</div>
+							</div>
+
+							<div class="row form-group">
+								<div class="col-md-12">
+									<label for="email">Email</label>
+									<input type="text" id="email" class="form-control" placeholder="Your email address">
+								</div>
+							</div>
+
+							<div class="row form-group">
+								<div class="col-md-12">
+									<label for="subject">Subject</label>
+									<input type="text" id="subject" class="form-control" placeholder="Your subject of this message">
+								</div>
+							</div>
+
+							<div class="row form-group">
+								<div class="col-md-12">
+									<label for="message">Message</label>
+									<textarea name="message" id="message" cols="30" rows="10" class="form-control" placeholder="Say something about us"></textarea>
+								</div>
+							</div>
+							<div class="form-group text-center">
+								<input type="submit" value="Send Message" class="btn btn-primary">
+							</div>
+
+						</form>		
+					</div>-->
+				</div>
+			</div>
+		</div>
 </html>
