7ee0624ad21821ffb8dada5c10df637c70306249
Samuil Georgiev <sami.georgiev@gmail.com>
2018-08-11 23:02:05 +0300

    new version

--- reviewsBG.php
+++ reviewsBG.php
@@ -106,28 +106,26 @@
 				</div>
 			</div>
 		</nav>
-
         <div id="disqus_thread"></div>
-    <script>
+        <script>
 
-    /**
-    *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-    *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+        /**
+        *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+        *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
 
-    var disqus_config = function () {
-    this.page.url = "http://centralwesthotel.com/reviews.php";  // Replace PAGE_URL with your page's canonical URL variable
-    this.page.identifier = "PAGE_IDENTIFIER"; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-    };
+        var disqus_config = function () {
+        this.page.url = "http://centralwesthotel.com/reviews.php";  // Replace PAGE_URL with your page's canonical URL variable
+        this.page.identifier = "PAGE_IDENTIFIER"; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+        };
 
-    (function() { // DON'T EDIT BELOW THIS LINE
-    var d = document, s = d.createElement('script');
-    s.src = 'https://sami-4.disqus.com/embed.js';
-    s.setAttribute('data-timestamp', +new Date());
-    (d.head || d.body).appendChild(s);
-    })();
-    </script>
-    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-
+        (function() { // DON'T EDIT BELOW THIS LINE
+        var d = document, s = d.createElement('script');
+        s.src = 'https://centralwesthotel.disqus.com/embed.js';
+        s.setAttribute('data-timestamp', +new Date());
+        (d.head || d.body).appendChild(s);
+        })();
+        </script>
+        <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
 	<footer id="colorlib-footer" role="contentinfo" style="padding-top: 20px">
 			<div class="container">
 				<div class="row row-pb-md">
