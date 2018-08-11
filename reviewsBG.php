f9270b7bcf9d839e7ce9eeb1fb7920169421096e
Samuil Georgiev <sami.georgiev@gmail.com>
2018-08-11 22:20:11 +0300

    bg version of reviews

---
+++ reviewsBG.php
@@ -0,0 +1,227 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Central West Hotel</title>
+	<link rel="shortcut icon" href="./logo.png">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+
+  <!-- Facebook and Twitter integration
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />-->
+
+	<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700" rel="stylesheet">
+
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="css/flexslider.css">
+
+	<!-- Owl Carousel -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+	<!-- Responsive design -->
+	<link rel="stylesheet" href="css/responsive.css">
+
+	<!-- Date Picker -->
+	<link rel="stylesheet" href="css/bootstrap-datepicker.css">
+	<!-- Flaticons  -->
+	<link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+
+	<div class="colorlib-loader"></div>
+
+	<div id="page">
+		<nav class="colorlib-nav" role="navigation">
+			<div class="top">
+				<div>
+					<div>
+						<!--<div class="col-xs-4">
+							<p class="site">www.centralwesthotel.com</p>
+						</div>-->
+
+					<div style = "margin-top:-8px">
+							<div class="header_site" >www.centralwesthotel.com</div>
+							<ul class="about">
+								<li><a href ="./contact.html#about-us">За Нас</a></li>
+								<li><a href ="./contact.html#reach">Как да стигнете</a></li>
+								<li><a href ="https://www.tripadvisor.com/Attractions-g294452-Activities-c47-Sofia_Sofia_Region.html" target="_blank">Забележителности</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="icons">
+					<a href="https://www.facebook.com/centralwesthotelsofia/" target = "_blank"><img src = "./images/facebook.png"></a>
+					<a href="index.php"><img src="images/en_flag.png" style="width: 30px"></a>
+					<a href="indexBG.php"><img src="images/bg-01.png" style="width: 30px"></a>
+				</div>
+			</div>
+			<div id="colorlib-logo" >
+				<a href="indexBG.php"><img src = "./logo.png" style="width:100%;height:100%;margin-bottom:20px auto 0 auto"></a>
+			</div>
+			<div class="top-menu">
+				<div class="container">
+					<div class="col-xs-10 tex-right menu-1">
+						<ul class="links">
+							<li><a href ="./indexBG.php" style = "word-spacing: 10px;font-size: 20px">Начало</a></li>
+							<li><a href="./roomsBG.html" style = "word-spacing: 10px;font-size: 20px">Стаи</a></li>
+							<li><a href="./amenitiesBG.html" style = "word-spacing: 10px;font-size: 20px">Удобства</a></li>
+							<li><a href="./galleryBG.html" style = "word-spacing: 10px;font-size: 20px">Галерия</a></li>
+							<li><a href="./contactBG.html" style = "word-spacing: 10px;font-size: 20px">Контакти</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</nav>
+
+        /**
+        *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+        *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+        /*
+        var disqus_config = function () {
+        this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+        this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+        };
+        */
+        (function() { // DON'T EDIT BELOW THIS LINE
+        var d = document, s = d.createElement('script');
+        s.src = 'https://sami-4.disqus.com/embed.js';
+        s.setAttribute('data-timestamp', +new Date());
+        (d.head || d.body).appendChild(s);
+        })();
+        </script>
+        <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+
+	<footer id="colorlib-footer" role="contentinfo" style="padding-top: 20px">
+			<div class="container">
+				<div class="row row-pb-md">
+					<div class="col-md-3 colorlib-widget" style="text-align:left">
+						<h4>Central West Hotel</h4>
+						<p style="color: #adabb1">„Central West Hotel“ е нов хотел, отворил врати в центъра на София през 2018г. Разположен в модерна сграда, около която можете да намерите множество ресторанти, заведения и магазини. Той разполага с 12 двойни стаи и 4 студия. Всички те са уютно обзаведени, като се състоят от дневна, спалня и просторна самостоятелна баня</p>
+						<p>
+							<!--<ul class="colorlib-social-icons">
+								<li><a href="#"><i class="icon-twitter"></i></a></li>
+								<li><a href="#"><i class="icon-facebook"></i></a></li>
+								<li><a href="#"><i class="icon-linkedin"></i></a></li>
+								<li><a href="#"><i class="icon-dribbble"></i></a></li>
+							</ul>-->
+						</p>
+					</div>
+					<div class="col-md-3 colorlib-widget" style="text-align:center">
+						<h4>Бързи линкове</h4>
+						<p>
+							<ul class="colorlib-footer-links">
+								<li><a href="./indexBG.php">Начало</a></li>
+								<li><a href="./roomsBG.html">Стаи</a></li>
+								<li><a href="./amenitiesBG.html">Удобства</a></li>
+								<li><a href="./galleryBG.html">Галерия</a></li>
+								<li><a href="./contactBG.html">Конатакти</a></li>
+							</ul>
+						</p>
+					</div>
+					<!--<div class="col-md-3">
+						<h4>Recents Blog Post</h4>
+						<ul class="colorlib-footer-links">
+							<li><a href="#">The Ultimate Packing List For Female Travelers</a></li>
+							<li><a href="#">How These 5 People Found The Path to Their Dream Trip</a></li>
+							<li><a href="#">A Definitive Guide to the Best Dining and Drinking Venues in the City</a></li>
+						</ul>
+					</div>-->
+
+					<div class="fixed_footer col-md-3 col-md-push-1">
+						<h4>Информация за контакти</h4>
+						<ul class="colorlib-footer-links">
+							<li>1309 София<br>бул. "Тодор Александров" 135</li>
+							<li><a href="tel://+359885747606">+359 885 747 606</a></li>
+							<li><a href="mailto:contact@centralwesthotel.com">contact@centralwesthotel.com</a></li>
+							<li><a href="https://centralwesthotel.com">centralwesthotel.com</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12 text-center">
+						<p>
+							<small class="block">
+								<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart3" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+							</small>
+							<small class="block">Demo Images: <a href="http://unsplash.co/" target="_blank">Unsplash</a></small>
+						</p>
+					</div>
+				</div>
+			</div>
+		</footer>
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up2"></i></a>
+	</div>
+
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+	<!-- Owl carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+	<!-- Date Picker -->
+	<script src="js/bootstrap-datepicker.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+
+	<!--Google maps -->
+	<script>
+		function myMap() {
+  			var myCenter = new google.maps.LatLng(42.701156,23.302300);
+  			var mapCanvas = document.getElementById("map");
+  			var mapOptions = {center: myCenter, zoom: 15};
+  			var map = new google.maps.Map(mapCanvas, mapOptions);
+  			var marker = new google.maps.Marker({position:myCenter});
+  			marker.setMap(map);
+}
+	</script>
+
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDg1_DVFO3m_r9FJg-k0l5TqWLplhNauDc&callback=myMap"></script>
+
+	</body>
+</html>
